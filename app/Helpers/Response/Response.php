ca7f88c656c4d667c542ed884e5f92128863a298
Mina <mina.r.meshriky@gmail.com>
2023-03-21 22:37:52 +0200

    adding a response class

---
+++ app/Helpers/Response/Response.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers\Response;
+
+class Response
+{
+    public const OK = 200;
+    public const NOTFOUND = 404;
+    public const UNPROCESSABLE_ENTITY = 422;
+    public const BAD_REQUEST = 400;
+    public const METHOD_NOT_ALLOWED = 405;
+    public const FATAL_ERROR = 500;
+
+    private ?string $content;
+    private int $code;
+
+
+    public function __construct()
+    {
+        $this->code = self::OK;
+        $this->content = null;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    public function send(): void
+    {
+        $this->applyHeader();
+        $this->applyContent();
+        die;
+    }
+
+    public function changeCode(int $code): self
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    public function changeContent(?array $content): self
+    {
+        $this->content = json_encode($content);
+        return $this;
+    }
+
+    private function applyHeader(): void
+    {
+        header("HTTP/1.1 {$this->code}");
+        http_response_code($this->code);
+    }
+
+    private function applyContent(): void
+    {
+        if ($this->content) {
+            echo $this->content;
+        }
+    }
+}
