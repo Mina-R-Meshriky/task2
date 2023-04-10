687b8353770dd789cb1ffdffe528160cf2a11c82
Mina Meshriky <mina.r.meshreky@gmail.com>
2023-04-10 16:59:02 +0200

    adding an auto_loaded functions file

--- app/Core/functions.php
+++ app/Core/functions.php
@@ -10,3 +10,10 @@ function ddj(...$var) {
     echo json_encode(count($var) == 1 ? $var[0] : $var);
     die;
 }
+
+function base_path($path): string {
+    return __DIR__."/../../$path";
+}
+function app_path($path): string {
+    return __DIR__."/../$path";
+}
